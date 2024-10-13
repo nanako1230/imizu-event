8f4860ffb5b2f4d6443123045896b70154f0f95b
nanako1230 <arinochao1230@gmail.com>
2024-10-13 18:07:32 +0900

    最終変更

--- src/pages/PosterPage2.js
+++ src/pages/PosterPage2.js
@@ -5,7 +5,7 @@ export default function PosterPage() {
   return (
     <div className="poster-page">
       <div className="poster-content">
-        <img src="\img\富山県立大学DXセンター.jpg" alt="イベントポスター" className="poster-image" />
+        <img src="\img\キャンペーンチラシ.jpg" alt="イベントポスター" className="poster-image" />
       </div>
     </div>
   );
